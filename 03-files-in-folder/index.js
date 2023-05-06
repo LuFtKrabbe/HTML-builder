dc2fdf4ee6efdc195bbf03575b05b428450b0cd4
Alexander Filimonov <filimonov.alexander.s@yandex.ru>
2023-05-07 00:05:08 +0300

    feat: solve the task 03-files-in-folder

---
+++ 03-files-in-folder/index.js
@@ -0,0 +1,19 @@
+const fs = require('fs');
+const path = require('path');
+
+const pathToCurrentFolder = path.dirname(__filename);
+const pathToSecretFolder = path.join(pathToCurrentFolder, 'secret-folder');
+
+process.stdout.write('Папка secret-folder содержит следующие файлы:\n');
+
+fs.readdir(pathToSecretFolder, {withFileTypes: true}, (err, files) => {
+  for (const file of files) {
+    if (file.isFile()) {
+      const fileExt = path.extname(file.name).slice(1);
+      const pathToSecretFile = path.join(pathToSecretFolder, file.name);
+      fs.stat(pathToSecretFile, (err, stats) => {
+        process.stdout.write(`${file.name} - ${fileExt} - ${stats.size/1000} kB\n`);
+      });
+    }
+  }
+});
