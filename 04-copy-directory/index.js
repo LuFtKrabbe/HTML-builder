378232be83d5bdd55c59da0aca968766f2b36e5e
Alexander Filimonov <filimonov.alexander.s@yandex.ru>
2023-05-07 22:54:47 +0300

    feat: solve the task 04-copy-directory

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,28 @@
+const fs = require('fs/promises');
+const path = require('path');
+
+const folderName = 'files-copy';
+const pathToCurrentFolder = path.dirname(__filename);
+const pathToFilesFolder = path.join(pathToCurrentFolder, 'files');
+const pathToFilesFolderCopy = path.join(pathToCurrentFolder, folderName);
+
+function copyDir() {
+
+  fs.rm(pathToFilesFolderCopy, { force: true, recursive: true })
+    .then(() => fs.mkdir(pathToFilesFolderCopy, { recursive: true }))
+    .then((pathToFolder) => console.log(`Next files have been copied or updated to ${pathToFolder}:`))
+    .then(() => fs.readdir(pathToFilesFolder, {withFileTypes: true}))
+    .then((files) => {
+      for (const file of files) {
+        if (!file.isFile()) {continue;}
+        const pathToFile = path.join(pathToFilesFolder, file.name);
+        const pathToFileCopy = path.join(pathToFilesFolderCopy, file.name);
+        fs.copyFile(pathToFile, pathToFileCopy);
+        console.log(`- ${file.name}`);
+      }
+    });
+  
+}
+
+copyDir();
 
