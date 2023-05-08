3eac4e0376ccc7299edb10d41d13e4f1705c65a6
Alexander Filimonov <filimonov.alexander.s@yandex.ru>
2023-05-08 20:04:37 +0300

    fix: correct display of the file's name

--- 03-files-in-folder/index.js
+++ 03-files-in-folder/index.js
@@ -10,9 +10,10 @@ fs.readdir(pathToSecretFolder, {withFileTypes: true}, (err, files) => {
   for (const file of files) {
     if (file.isFile()) {
       const fileExt = path.extname(file.name).slice(1);
+      const fileName = file.name.slice(0, file.name.indexOf('.'));
       const pathToSecretFile = path.join(pathToSecretFolder, file.name);
       fs.stat(pathToSecretFile, (err, stats) => {
-        process.stdout.write(`${file.name} - ${fileExt} - ${stats.size/1000} kB\n`);
+        process.stdout.write(`${fileName} - ${fileExt} - ${stats.size/1000} kB\n`);
       });
     }
   }
